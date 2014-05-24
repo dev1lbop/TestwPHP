ebd1860f44f21e63c216835de3e9b6a4f6083760
Jada Potata <williamsj26@udayton.edu>
2014-05-24 13:25:08 -0400

    with index
    added the index file for the companies stuff

---
+++ index.php
@@ -0,0 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+<meta name="date" content="2014-05-24T13:19:00-0500" >
+<meta name="copyright" content="TMIT World Limited">
+<meta name="keywords" content="Infinite Skills - php/MySQL Training">
+<meta name="description" content="Infinite Skills - php/MySQL Training">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+
+</head>
+<body>
+
+<?php
+	
+	echo '<h2 style="font-family: arial, helvetica, sans-serif;" >
+				Infinite Skills - php/MySQL Training
+			</h2>';
+	echo '<div align="left",
+				  style="
+				  font-family: arial, helvetica, sans-serif; 
+				  font-style: sans-serif; 
+				  padding-left: 50px;
+			">';
+				
+		echo '<a href="companyListOrder.php">
+				<span style="color: maroon; ">List Companies</span>
+				</a>';
+		echo '<br /><br />';
+
+
+				
+		echo '<a href="companyCreate.php">
+				<span style="color: maroon; ">Create New company</span>
+				</a>';
+		echo '<br /><br />';
+//Link to the company edit script below
+		echo '<a href="companyEdit.php">
+				<span style="color: maroon; ">Edit company</span>
+				</a>';
+		echo '<br /><br />';
+
+//Link to the company select script below
+		echo '<a href="companyPeopleEdit.php">
+				<span style="color: maroon; ">List People in a Company</span>
+				</a>';
+		echo '<br /><br />';
+
+	echo '</div>';
+
+?>
+
+
+</body>
+</html>
