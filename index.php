6bae4dbd3ed06736b260508cdde493a1c6d0eba6
Jada Potata <williamsj26@udayton.edu>
2014-05-24 22:39:06 -0400

    index
    removing uneccesary  code and unused links

--- index.php
+++ index.php
@@ -11,26 +11,20 @@
 				  padding-left: 50px;
 			">';
 				
-		echo '<a href="companyListOrder.php">
-				<span style="color: maroon; ">List Companies</span>
-				</a>';
-		echo '<br /><br />';
-
-
-				
-		echo '<a href="companyCreate.php">
+					
+		/*echo '<a href="companyCreate.php">
 				<span style="color: maroon; ">Create New company</span>
 				</a>';
-		echo '<br /><br />';
+		echo '<br /><br />';*/
 //Link to the company edit script below
 		echo '<a href="companyEdit.php">
-				<span style="color: maroon; ">Edit company</span>
+				<span style="color: maroon; ">Edit Country</span>
 				</a>';
 		echo '<br /><br />';
 
 //Link to the company select script below
 		echo '<a href="companyPeopleEdit.php">
-				<span style="color: maroon; ">List People in a Company</span>
+				<span style="color: maroon; ">List Countries</span>
 				</a>';
 		echo '<br /><br />';
 
