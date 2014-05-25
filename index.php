b4ab7b7d719875cfdc58b17d35429f655be8bb7f
Jada Potata <williamsj26@udayton.edu>
2014-05-25 09:44:35 -0400

    index
    added a different path to the files so they can be referenced

--- index.php
+++ index.php
@@ -1,34 +1,55 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+
+<meta name="date" content="2014-05-24T13:19:00-0500" >
+<meta name="copyright" content="TMIT World Limited">
+<meta name="keywords" content="Infinite Skills - php/MySQL Training">
+<meta name="description" content="Infinite Skills - php/MySQL Training">
+<meta name="ROBOTS" content="NOINDEX, NOFOLLOW">
+
+</head>
+<body>
 
 <?php
 	
 	echo '<h2 style="font-family: arial, helvetica, sans-serif;" >
-				Microgrid database Test
+				Infinite Skills - php/MySQL Training
 			</h2>';
-	echo '<div align="left",
+	echo '<div align="left",
 				  style="
 				  font-family: arial, helvetica, sans-serif; 
 				  font-style: sans-serif; 
 				  padding-left: 50px;
 			">';
 				
-					
-		/*echo '<a href="companyCreate.php">
+		echo '<a href="../companyListOrder.php">
+				<span style="color: maroon; ">List Companies</span>
+				</a>';
+		echo '<br /><br />';
+
+
+				
+		echo '<a href="../companyCreate.php">
 				<span style="color: maroon; ">Create New company</span>
 				</a>';
-		echo '<br /><br />';*/
+		echo '<br /><br />';
 //Link to the company edit script below
 		echo '<a href="companyEdit.php">
-				<span style="color: maroon; ">Edit Country</span>
+				<span style="color: maroon; ">Edit company</span>
 				</a>';
 		echo '<br /><br />';
 
 //Link to the company select script below
-		echo '<a href="companyPeopleEdit.php">
-				<span style="color: maroon; ">List Countries</span>
+		echo '<a href="../companyPeopleEdit.php">
+				<span style="color: maroon; ">List People in a Company</span>
 				</a>';
 		echo '<br /><br />';
 
 	echo '</div>';
-
+//Link to another page 
 ?>
 
+
+</body>
+</html>
